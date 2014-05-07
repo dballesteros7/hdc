0d42a22b80b50642db4910526974699189f9e53d
ruiixu23 <ruifeng.x.cn@gmail.com>
2014-05-07 11:01:17 +0200

    added fetching movement data script

--- utils/jawbone-up.py
+++ utils/jawbone-up.py
@@ -1,5 +1,6 @@
 from datetime import date
 import json
+import os
 
 from requests_oauthlib import OAuth2Session
 
@@ -11,19 +12,25 @@ redirect_uri = 'https://localhost:8080/redirect/jawbone-up'
 scope = ['basic_read extended_read location_read friends_read '
          'mood_read move_read sleep_read meal_read weight_read '
          'cardiac_read generic_event_read']
-home = '/tmp/'
+home = '/tmp/hdc/'
+
+sleep_list_url = 'https://jawbone.com/nudge/api/v.1.1/users/@me/sleeps'
+sleep_ticks_url = 'https://jawbone.com/nudge/api/v.1.1/sleeps/%s/ticks'
+move_list_url = 'https://jawbone.com/nudge/api/v.1.1/users/@me/moves'
+move_ticks_url = 'https://jawbone.com/nudge/api/v.1.1/moves/%s/ticks'
+
 
 if no_token:
-    jawboneup = OAuth2Session(client_id, redirect_uri=redirect_uri,
+    jawbone_up = OAuth2Session(client_id, redirect_uri=redirect_uri,
                           scope=scope)
-    authorization_url, state = jawboneup.authorization_url(
+    authorization_url, state = jawbone_up.authorization_url(
                                         'https://jawbone.com/auth/oauth2/auth')
 
     print 'Please go to link below and authorize access.\n%s ' % authorization_url
 
     authorization_response = raw_input('Enter the full callback URL:\n')
 
-    token = jawboneup.fetch_token('https://jawbone.com/auth/oauth2/token',
+    token = jawbone_up.fetch_token('https://jawbone.com/auth/oauth2/token',
                                   authorization_response=authorization_response,
                                   client_secret=client_secret)
 
@@ -32,22 +39,41 @@ if no_token:
 else:
     access_token = 'b6_3pfGGwEgiSSivKqnmQfFK59dIENPZrQgq9fo9MosBIJirwCCGrd86BCNCwDk0ckgNEEjEXsdBvBmPfMJql1ECdgRlo_GULMgGZS0EumxrKbZFiOmnmAPChBPDZ5JP'
     refresh_token = 'oAiwG8gfPrCLpr_xOljdZrRWJfxTwsW1gltsDCH6KQvdL2WxNgQvKekaCy5aBtavNNWfJhnfRQwlAN2iCODyqw'
-    jawboneup = OAuth2Session(client_id, token={'refresh_token' : refresh_token,
+    jawbone_up = OAuth2Session(client_id, token={'refresh_token' : refresh_token,
                                                 'access_token' : access_token})
 
-
-def get_sleep(query_date):
-    r = jawboneup.get('https://jawbone.com/nudge/api/v.1.1/users/@me/sleeps',
-                      params={'date' : query_date.strftime('%Y%m%d')})
-    base_result = r.json()
+def get_move_data(query_date):
+    response = jawbone_up.get(move_list_url, params={'date':query_date.strftime('%Y%m%d')})
+    base_result = response.json()
     for item in base_result['data']['items']:
         xid = item['xid']
-        extra = jawboneup.get(
-                    'https://jawbone.com/nudge/api/v.1.1/sleeps/%s/ticks' % xid)
+        extra = jawbone_up.get(move_ticks_url % xid)
         base_result[xid] = extra.json()
     return base_result
 
-for val in xrange(1, 32):
-    out_file = open(home + 'sleep03%s' % val, 'w');
-    json.dump(get_sleep(date(2014, 3, val)), out_file)
+def get_sleep_data(query_date):
+    r = jawbone_up.get(sleep_list_url, params={'date' : query_date.strftime('%Y%m%d')})
+    base_result = r.json()
+    for item in base_result['data']['items']:
+        xid = item['xid']
+        extra = jawbone_up.get(sleep_ticks_url % xid)
+        base_result[xid] = extra.json()
+    return base_result
+
+year = 2014
+month = 3
+os.system('mkdir -p ' + home)
+for day in xrange(1, 32):
+    date_to_get = date(year, month, day)
+    print 'Fetching data on date %s' % date_to_get
+    
+    out_file = open(home + 'sleep-%s' % date_to_get, 'w')
+    json.dump(get_sleep_data(date_to_get), out_file, indent=2)
     out_file.close()
+
+    out_file = open(home + 'move-%s' % date_to_get, 'w')
+    json.dump(get_move_data(date_to_get), out_file, indent=2)
+    out_file.close()
+print 'Finished fetching data'
+    
+    
